981bc5e9b2dd0ebce016bad9046bc19384b2903e
MAXENCE VANBOCKSTAELE <maxence.vanbockstaele@student.vinci.be>
2023-01-20 10:54:31 +0100

    feat: add check app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 try {
-    const { isEmpty } = require("./services/validations");
+    const { isEmpty, checkGamerTag } = require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -13,9 +13,8 @@ try {
     });
 
     checkButton.addEventListener("click", () => {
-        // TODO: Add the logic to display the correct feedback message (error and validation)
-        feedbackMessageText = isEmpty(gamerTagValue)
-            ? "Gamer tag cannot be empty"
+        feedbackMessageText = isEmpty(gamerTagValue) && checkGamerTag(gamerTagValue)
+            ? "Gamer tag doesnt match"
             : "Gamer tag is valid";
         feedbackMessage.textContent = feedbackMessageText;
     });
